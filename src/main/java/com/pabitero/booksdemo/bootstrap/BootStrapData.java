ad3c3f8aa3f89f24da921035f9d5fb06b3b1b57f
William <pabiterowilliam@yahoo.com>
2020-12-07 16:43:23 -0500

    Fixed issue where only one author could be added to a book

--- src/main/java/com/pabitero/booksdemo/bootstrap/BootStrapData.java
+++ src/main/java/com/pabitero/booksdemo/bootstrap/BootStrapData.java
@@ -8,8 +8,7 @@ import com.pabitero.booksdemo.service.BookService;
 import com.pabitero.booksdemo.service.PublisherService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
@@ -24,45 +23,73 @@ public class BootStrapData implements CommandLineRunner {
         this.publisherService = publisherService;
     }
 
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
         Author will = new Author("William", "Pabitero");
-        Book book1 = new Book("How to Code", "1112");
-        Publisher pub1 = new Publisher("pub1", "67-14 41st Ave", "Woodside", "NY", "11377");
-        publisherService.savePublisher(pub1);
-
-        will.getBooks().add(book1);
-        book1.getAuthors().add(will);
-
-        book1.setPublisher(pub1);
-        pub1.getBooks().add(book1);
-
         Author elon = new Author("Elon", "Musk");
         Author bill = new Author("Bill", "Gates");
         Author jeff = new Author("Jeff", "Bezos");
-        Book book2 = new Book("Super Rocket Manual", "123423");
-        Book book3 = new Book("Microsoft: The Legacy", "3412");
-        Publisher pub2 = new Publisher("NY Publishing", "41st W", "New York", "NY", "11153");
-        publisherService.savePublisher(pub2);
-
+        authorService.saveAuthor(will);
         authorService.saveAuthor(elon);
         authorService.saveAuthor(bill);
         authorService.saveAuthor(jeff);
 
+        Book book1 = new Book("How to Code", "1112", "Learn how to code in java using spring framework");
+        Book book2 = new Book("Super Rocket Manual", "123423", "Guide to building a huge rocket");
+        Book book3 = new Book("Microsoft: The Legacy", "3412", "History of Microsoft");
+        bookService.saveBook(book1);
+        bookService.saveBook(book2);
+        bookService.saveBook(book3);
+
+        Publisher pub1 = new Publisher("Pabs Publishing", "67-14 41st Ave", "Woodside", "NY", "11377");
+        Publisher pub2 = new Publisher("NY Publishing", "41st W", "New York", "NY", "11153");
+        publisherService.savePublisher(pub1);
+        publisherService.savePublisher(pub2);
+
+        will.getBooks().add(book1);
+        book1.getAuthors().add(will);
+        will.getBooks().add(book2);
+        book2.getAuthors().add(will);
+        will.getBooks().add(book3);
+        book3.getAuthors().add(will);
+
+        elon.getBooks().add(book2);
+        book2.getAuthors().add(elon);
+        elon.getBooks().add(book1);
+        book1.getAuthors().add(elon);
+
+        bill.getBooks().add(book3);
+        book3.getAuthors().add(bill);
+
+        jeff.getBooks().add(book2);
+        book2.getAuthors().add(jeff);
+
+        book1.setPublisher(pub1);
+        book2.setPublisher(pub2);
+        book3.setPublisher(pub2);
+        pub1.getBooks().add(book1);
+        pub2.getBooks().add(book2);
+        pub2.getBooks().add(book3);
+
+        bookService.saveBook(book1);
         bookService.saveBook(book2);
         bookService.saveBook(book3);
 
         authorService.saveAuthor(will);
-        bookService.saveBook(book1);
+        authorService.saveAuthor(elon);
+        authorService.saveAuthor(bill);
+        authorService.saveAuthor(jeff);
+
         publisherService.savePublisher(pub1);
+        publisherService.savePublisher(pub2);
 
         System.out.println("Started in bootstrap");
         System.out.println("Number of books " + bookService.getBooks().size());
         System.out.println("Publisher Count " + publisherService.getPublishers().size());
         System.out.println("Publisher 1 Number of Books " + pub1.getBooks().size());
-
-//        List<Author> authors = authorService.getAuthors();
-//        authors.stream().forEach(author -> System.out.println(author.getBooks().toString()));
+        bookService.getBooks().forEach(book -> System.out.println(book.getAuthors()));
+        authorService.getAuthors().forEach(author -> System.out.println(author.getBooks()));
 
     }
 }
