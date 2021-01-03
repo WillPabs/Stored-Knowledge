ee654d52f4a8313d9b7db36f0826f6ea787cdcc0
William <pabiterowilliam@yahoo.com>
2021-01-03 17:15:03 -0500

    Updated url paths and added logging in controllers. Adjusted for proper path in html

--- src/main/java/com/pabitero/booksdemo/controller/AuthorController.java
+++ src/main/java/com/pabitero/booksdemo/controller/AuthorController.java
@@ -1,50 +1,68 @@
 package com.pabitero.booksdemo.controller;
 
 import com.pabitero.booksdemo.entity.Author;
+import com.pabitero.booksdemo.entity.Book;
 import com.pabitero.booksdemo.service.AuthorService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
+@RequestMapping("/authors")
+@Slf4j
 public class AuthorController {
 
     @Autowired
-    private final AuthorService authorService;
+    private AuthorService authorService;
 
-    public AuthorController(AuthorService authorService) {
-        this.authorService = authorService;
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAuthorsBooks(@PathVariable(required = false) Long id,
+                                  Model model) {
+        log.info("Inside getAuthorsBooks in AuthorController");
+
+        if(id == null) {
+            model.addAttribute("authors", authorService.getAuthors());
+            return "books/authorList";
+
+        } else if(authorService.getAuthorById(id) != null) {
+            Author existingAuthor = authorService.getAuthorById(id);
+            model.addAttribute("author", existingAuthor);
+            model.addAttribute("books", existingAuthor.getBooks());
+            return "books/authorBooks";
+
+        } else {
+            return "books/errorPage";
+        }
     }
 
-    @RequestMapping("/authors")
-    public String getAuthors(Model model) {
-        model.addAttribute("authors", authorService.getAuthors());
-
-        return "books/authorList";
-    }
-
-    @GetMapping("/authors-create")
+    @GetMapping("/create")
     public String createAuthor(Model model) {
+        log.info("Inside createAuthor in AuthorController");
         model.addAttribute("author", new Author());
 
         return "books/authorCreator";
     }
 
-    @PostMapping("/saveAuthor")
-    public String saveNewAuthor(@ModelAttribute Author author) {
+    @PostMapping("/")
+    public String saveNewAuthor(@RequestBody Author author) {
+        log.info("Inside saveNewAuthor in AuthorController");
         authorService.saveAuthor(author);
 
         return "books/result";
     }
 
-    @GetMapping("/authors/{id}/books")
-    public String getAuthorsBooks(@PathVariable Long id, Model model) {
-        Author existingAuthor = authorService.getAuthorById(id);
-        if(existingAuthor != null) {
-            model.addAttribute("author", existingAuthor);
-            model.addAttribute("books", existingAuthor.getBooks());
+    @PutMapping("/")
+    public String updateAuthor(@RequestBody Author author) {
+        log.info("Inside updateAuthor in AuthorController");
+        if(author.getId() != null) {
+            authorService.updateAuthor(author);
+            return "redirect:/authors";
+        } else {
+            return "books/errorPage";
         }
-        return "books/authorBooks";
     }
 }
--- src/main/java/com/pabitero/booksdemo/controller/BookController.java
+++ src/main/java/com/pabitero/booksdemo/controller/BookController.java
@@ -1,26 +1,48 @@
 package com.pabitero.booksdemo.controller;
 
-import com.pabitero.booksdemo.repository.BookRepository;
+import com.pabitero.booksdemo.entity.Book;
 import com.pabitero.booksdemo.service.BookService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/books")
+@Slf4j
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
-
-    @RequestMapping("/books")
+    @GetMapping
     public String getBooks(Model model) {
+        log.info("Inside getBooks in BookController");
         model.addAttribute("books", bookService.getBooks());
         return "books/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getBookById(Model model, @PathVariable Long id) {
+        log.info("Inside getBookById in BookController");
+        Book foundBook;
+        try {
+            foundBook = bookService.getBookById(id);
+
+            if(!foundBook.equals(Book.class) || foundBook == null) {
+                return "books/errorPage";
+            } else {
+                model.addAttribute("book", foundBook);
+                model.addAttribute("authors", foundBook.getAuthors());
+                model.addAttribute("publisher", foundBook.getPublisher());
+                return "books/bookPage";
+            }
+        } catch (NullPointerException e) {
+            log.error(e.getMessage());
+        }
+        return "books/errorPage";
     }
 }
