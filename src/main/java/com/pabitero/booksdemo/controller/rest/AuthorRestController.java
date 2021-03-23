943de78eddd9a90719a4e786b095bc7bac99cae5
William <pabiterowilliam@yahoo.com>
2021-03-23 17:26:25 -0400

    Added JSON REST Controllers

---
+++ src/main/java/com/pabitero/booksdemo/controller/rest/AuthorRestController.java
@@ -0,0 +1,30 @@
+package com.pabitero.booksdemo.controller.rest;
+
+import com.pabitero.booksdemo.entity.Author;
+import com.pabitero.booksdemo.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/authors")
+@CrossOrigin("*")
+public class AuthorRestController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorRestController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> getAuthors() {
+        return authorService.getAuthors();
+    }
+
+}
